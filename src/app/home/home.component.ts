37091aeadc09c5c66041dd3c1b2533cf0cbbb854
SeanDeeze <davidseandunn@gmail.com>
2018-11-27 18:21:32 -0800

    remove constant pinging from home.component
    Updated to unsubscribe from active Player querying

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IPlayer } from '../shared/models/player';
 import { LoginService } from '../services/login.service';
 import { interval } from 'rxjs/observable/interval';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   activePlayers: IPlayer[] = [];
   interval = 5000;
+  source: Subscription;
   constructor(private _loginService: LoginService) { }
 
   ngOnInit() {
-    interval(this.interval).subscribe(() => {
+     this.source = interval(this.interval).subscribe(() => {
       if (this._loginService.isPlayerLoggedIn()) {
         this.updateActivePlayers();
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.source.unsubscribe();
   }
 
   updateActivePlayers() {
