c93e80a5f3bd1a4dd36dd2787f81878f09dfc1b3
SDeeze <davidseandunn@gmail.com>
2020-08-03 21:17:39 -0700

    updated packages

--- CardGameAPI/CardGameAPI/CardGameUI/src/environments/environment.prod.ts
+++ CardGameAPI/CardGameAPI/CardGameUI/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'http://104.168.143.158/CardGame/api/',
-  signalR: 'http://104.168.143.158/CardGame/',
   imageBase: 'http://104.168.143.158/CardGame/assets/images/'
 };
