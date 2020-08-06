97dec2fdee14d5af65cc9bf0f25c117abebe7540
SDeeze <davidseandunn@gmail.com>
2020-08-05 19:37:40 -0700

    standardize the startup.cs file updated node version to make VS happy

--- CardGameAPI/CardGameAPI/Startup.cs
+++ CardGameAPI/CardGameAPI/Startup.cs
@@ -2,75 +2,75 @@ using CardGameAPI.Models;
 using CardGameAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
-using System.IO;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CardGameAPI
 {
-  public class Startup
-  {
-    public IConfiguration Configuration { get; }
-
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
+        readonly string CORS_POLICY = "CorsPolicy";
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddOptions();
-      services.AddCors(options =>
-      {
-        options.AddPolicy("CorsPolicy",
-            builder => builder.WithOrigins("http://localhost:4200")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials());
-      });
+        public IConfiguration Configuration { get; }
 
-      services.AddDbContext<EFContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
-
-
-      services.AddSingleton<IGameEngine, GameEngine>(s =>
-        new GameEngine(new EFContext(new DbContextOptionsBuilder<EFContext>()
-                                      .UseSqlServer(Configuration.GetConnectionString("DbConnection")).Options)));
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseHsts();
-      }
-
-      app.UseCors("CorsPolicy");
-      app.UseHttpsRedirection();
-
-      app.Use(async (context, next) => // Add dynamic redirection for angular-based requests
-      {
-        await next();
-
-        if (context.Response.StatusCode == 404 &&
-            !Path.HasExtension(context.Request.Path.Value) &&
-            !context.Request.Path.Value.StartsWith("/api/"))
+        public Startup(IConfiguration configuration)
         {
-          context.Request.Path = "/index.html";
-          await next();
+            Configuration = configuration;
         }
-      });
 
-      app.UseDefaultFiles(new DefaultFilesOptions { DefaultFileNames = new List<string> { "index.html" } });
-      app.UseMvc();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+              builder => builder.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials());
+            });
+
+            services.AddSingleton<IGameEngine, GameEngine>(s =>
+              new GameEngine(new EFContext(new DbContextOptionsBuilder<EFContext>()
+                                            .UseSqlServer(Configuration.GetConnectionString("DbConnection")).Options)));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
+            app.UseCors(CORS_POLICY);
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseExceptionHandler("/Error");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "./CardGameUI/dist";
+
+                if (env.IsDevelopment())
+                {
+                    spa.Options.StartupTimeout = new TimeSpan(0, 0, 80);
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
     }
-  }
 }
