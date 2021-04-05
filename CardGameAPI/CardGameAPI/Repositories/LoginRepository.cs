91c4d27e3e5d21825ec0d0fc65fc5123fa6267b6
Sean Dunn <davidseandunn@gmail.com>
2021-04-05 16:54:31 +0200

    Minor code cleanup

--- CardGameAPI/CardGameAPI/Repositories/LoginRepository.cs
+++ CardGameAPI/CardGameAPI/Repositories/LoginRepository.cs
@@ -1,7 +1,6 @@
 using CardGameAPI.Controllers;
 using CardGameAPI.Models;
 using CardGameAPI.Models.Dto;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,80 +8,85 @@ using System.Linq;
 
 namespace CardGameAPI.Repositories
 {
-  public class LoginRepository
-  {
-    private readonly EFContext _context;
-    private readonly IGameEngine _gameEngine;
-    private readonly ILogger<LoginController> _logger;
-
-    public LoginRepository(EFContext context, IGameEngine gameEngine, ILogger<LoginController> logger)
+    public class LoginRepository
     {
-      _context = context;
-      _gameEngine = gameEngine;
-      _logger = logger;
-    }
+        private readonly EFContext _context;
+        private readonly IGameEngine _gameEngine;
+        private readonly ILogger<LoginController> _logger;
 
-    public CGMessage Login(Player player)
-    {
-      CGMessage returnMessage = new CGMessage();
-      try
-      {
-        Player currentPlayer = _gameEngine.GetPlayers().FirstOrDefault(p => p.UserName.ToLower().Equals(player.UserName.Trim().ToLower()));
-        if (currentPlayer != null) // Player is active, just return that info
+        public LoginRepository(EFContext context, IGameEngine gameEngine, ILogger<LoginController> logger)
         {
-          currentPlayer.LastActivity = DateTime.Now;
-          _context.SaveChanges();
-          returnMessage.ReturnData.Add(currentPlayer);
+            _context = context;
+            _gameEngine = gameEngine;
+            _logger = logger;
         }
-        else // Initial Login, Update login activity and add to GameEngine
-        {
-          Player dbPlayer = _context.Players.FirstOrDefault(p => p.UserName.ToLower().Equals(player.UserName.Trim().ToLower())); // Does user exist in DB
-          if (dbPlayer == null)
-          {
-            _context.Players.Add(player);
-            _context.SaveChanges();
-            dbPlayer = _context.Players.FirstOrDefault(p => p.UserName.ToLower().Equals(player.UserName.Trim().ToLower()));
-          }
 
-          if (dbPlayer != null)
-          {
-            dbPlayer.LastActivity = DateTime.Now;
-            _gameEngine.GetPlayers().Add(dbPlayer);
-            returnMessage.ReturnData.Add(dbPlayer);
-          }
-        }
-        returnMessage.Status = true;
-      }
-      catch (Exception ex)
-      {
-        _logger.Log(LogLevel.Error, $"Method:Login; Error: {ex.Message}", returnMessage);
-      }
-      return returnMessage;
-    }
-
-    public CGMessage Logout(Player player)
-    {
-      CGMessage returnMessage = new CGMessage();
-      try
-      {
-        Player currentPlayer = _gameEngine.GetPlayers().FirstOrDefault(p => p.Id.Equals(player.Id));
-        if (currentPlayer != null)
+        public CGMessage Login(Player player)
         {
-          _gameEngine.GetPlayers().Remove(currentPlayer);
-          List<Player> players = _gameEngine.GetLoggedInUsers();
-          returnMessage.ReturnData.Add(players);
-          foreach (Game g in _gameEngine.GetGames())
-          {
-            g.Players.RemoveAll(p => p.Id.Equals(player.Id));
-          }
-          returnMessage.Status = true;
+            CGMessage returnMessage = new CGMessage();
+            try
+            {
+                Player currentPlayer = _gameEngine.GetPlayers().FirstOrDefault(p => p.UserName.ToLower().Equals(player.UserName.Trim().ToLower()));
+                if (currentPlayer != null) // Player is active, just return that info
+                {
+                    currentPlayer.LastActivity = DateTime.Now;
+                    _context.SaveChanges();
+                    returnMessage.ReturnData.Add(currentPlayer);
+                }
+                else // Initial Login, Update login activity and add to GameEngine
+                {
+                    Player dbPlayer = _context.Players.FirstOrDefault(p => p.UserName.ToLower().Equals(player.UserName.Trim().ToLower())); // Does user exist in DB
+                    if (dbPlayer == null)
+                    {
+                        _context.Players.Add(player);
+                        _context.SaveChanges();
+                        dbPlayer = _context.Players.FirstOrDefault(p => p.UserName.ToLower().Equals(player.UserName.Trim().ToLower()));
+                    }
+
+                    if (dbPlayer != null)
+                    {
+                        dbPlayer.LastActivity = DateTime.Now;
+                        _gameEngine.GetPlayers().Add(dbPlayer);
+                        returnMessage.ReturnData.Add(dbPlayer);
+                    }
+                }
+                returnMessage.Status = true;
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, $"Method:Login; Error: {ex.Message}", returnMessage);
+            }
+            return returnMessage;
         }
-      }
-      catch (Exception ex)
-      {
-        _logger.Log(LogLevel.Error, $"Method:Logout; Error: {ex.Message}", returnMessage);
-      }
-      return returnMessage;
+
+        public CGMessage Logout(Player player)
+        {
+            CGMessage returnMessage = new CGMessage();
+            try
+            {
+                if (player == null)
+                {
+                    return returnMessage;
+                }
+
+                Player currentPlayer = _gameEngine.GetPlayers().FirstOrDefault(p => p.Id.Equals(player.Id));
+                if (currentPlayer != null)
+                {
+                    _gameEngine.GetPlayers().Remove(currentPlayer);
+                    List<Player> players = _gameEngine.GetLoggedInUsers();
+                    returnMessage.ReturnData.Add(players);
+                    foreach (Game g in _gameEngine.GetGames())
+                    {
+                        g.Players.RemoveAll(p => p.Id.Equals(player.Id));
+                    }
+                    returnMessage.Status = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, $"Method:Logout; Error: {ex.Message}", returnMessage);
+            }
+            return returnMessage;
+        }
     }
-  }
 }
